0eac9b42dbacb1611430c46d51fe28008563396b
axl <you@example.com>
2024-10-22 14:12:56 -0300

    Hotfixes login

--- page/scripts/sesion.js
+++ page/scripts/sesion.js
@@ -1,18 +1,22 @@
 window.onload = () => {
-    
+
 }
 
 function logIn() {
     let user = document.getElementById("usuario").value;
     let pass = document.getElementById("contraseña").value;
 
-    if (checkLoggedIn()) {
-        alert("Ya estas logeado.");
+    if (!user || !pass) {
+        alert("Faltan datos. Verifique.")
     }
     else if (user === "TestUser" && pass === "123test") {
-        localStorage.username = user;
-        window.location.href = "../gestion/gestion.html";
-        console.log(localStorage.username);
+        if (checkLoggedIn()) {
+            alert("Ya estas logeado.");
+        } else {
+            localStorage.username = user;
+            window.location.href = "../gestion/gestion.html";
+            console.log(localStorage.username);
+        }
     }
     else
         alert("Error al iniciar sesion")
