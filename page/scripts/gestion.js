c69108e8047472793fad51af9a313487f00105ca
emi <your@email.com>
2024-11-05 15:41:16 -0300

    pn

--- page/scripts/gestion.js
+++ page/scripts/gestion.js
@@ -1,4 +1,35 @@
-const logged = true; // chequear cookie o algo para verificar si esta logeado
+import GameDAO from "../../dao/gameDAO.js";
 
-if (!logged) //redireccionar a pagina de error
-    console.log("asd");
+window.onload = async()=>{
+let juegos = await obtenerJuegos();
+mostrarJuegos (juegos);
+}
+
+async function obtenerJuegos(){
+    let gameDAO = new GameDAO();
+    let respuesta = await gameDAO.obtenerJuegos("fede");
+    console.log(respuesta);
+    return respuesta.data;
+}
+
+function mostrarJuegos(juegos){
+    let datosTablaElement = document.querySelector("#datosTabla");
+    datosTablaElement.innerHTML = "";
+    juegos.forEach( (juego) =>{
+        datosTablaElement.innerHTML += `
+        <tr>        
+          <td>${juego.nombre}</td>
+          <td>${juego.descripcion}</td>
+        </tr> <img class="imgTabla" src="${juego.urlImagen}"></td>
+        `;
+        console.log(juego); 
+    });
+
+    console.log(juegos);
+
+}
+
+
+
+
+
